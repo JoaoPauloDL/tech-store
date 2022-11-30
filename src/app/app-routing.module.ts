150c0939878f8829fad085cec1a6b788bb1e645e
João Paulo <joaopaulomayka@gmail.com>
2022-11-30 19:13:42 -0300

    criação do redirecionamento da pagina não encontrada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 
 const routes: Routes = [{ path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
-{path: "", redirectTo: "produtos", pathMatch: "full"}// quando a raiz do site for carregada esse componente será aberto
+{path: "", redirectTo: "produtos", pathMatch: "full"},// quando a raiz do site for carregada esse componente será aberto
+{path:"**", component: NaoEncontradoComponent }
 ];
 
 @NgModule({
