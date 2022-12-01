272b4fd7d8a172cf49806569610d95cf4c3ca956
João Paulo <joaopaulomayka@gmail.com>
2022-12-01 15:30:24 -0300

    implementação do produtosService

--- src/app/produtos/detalhes-produtos/detalhes-produtos.component.ts
+++ src/app/produtos/detalhes-produtos/detalhes-produtos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduto } from 'src/app/produtos';
+import { ProdutosService } from 'src/app/produtos.service';
 
 @Component({
   selector: 'app-detalhes-produtos',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalhesProdutosComponent implements OnInit {
 
-  constructor() { }
+  produto: IProduto | undefined;
+  quantidade = 1;
+
+  constructor(
+    private produtoService: ProdutosService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const produtoId = Number(routeParams.get("id"));
+    this.produto = this.produtoService.getOne(produtoId);
   }
 
 }
