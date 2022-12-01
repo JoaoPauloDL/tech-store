25ac3143860a3650a5007fe463e053e729b17cba
João Paulo <joaopaulomayka@gmail.com>
2022-12-01 14:44:15 -0300

    criação da tela de detalhes do produto

--- src/app/produtos/produtos-routing.module.ts
+++ src/app/produtos/produtos-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetalhesProdutosComponent } from './detalhes-produtos/detalhes-produtos.component';
 import { ProdutosComponent } from './produtos.component';
 
-const routes: Routes = [{ path: '', component: ProdutosComponent }];
+const routes: Routes = [
+  { path: '', component: ProdutosComponent },
+  {path:':id', component: DetalhesProdutosComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
